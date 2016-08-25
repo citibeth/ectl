478d8119f3f7af1b9286c7282ca5b220b0488a86
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-08-25 00:05:27 -0400

    Added missing files

---
+++ lib/ectl/gissdate.py
@@ -0,0 +1,30 @@
+import datetime
+import re
+
+monthRE_pat = r'(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(\d\d\d\d)'
+monthRE = re.compile(monthRE_pat)
+_str_to_month = {
+    'JAN' : 1,'FEB' : 2,'MAR' : 3,'APR' : 4,'MAY' : 5,'JUN' : 6,
+    'JUL' : 7,'AUG' : 8,'SEP' : 9,'OCT' : 10,'NOV' : 11,'DEC' : 12}
+
+def str_to_month(smon):
+    return _str_to_month[smon]
+
+dateRE = re.compile(r'(\d+)' + monthRE_pat)
+
+
+def monthstr_to_date(ms):
+    month = _monthnumbs[ms[0:3]]
+    year = int(ms[3:])
+    return datetime.date(month,year)
+
+def monthnum(year, month):
+    return year * 12 + (month - 1)
+
+def date_to_monthnum(date):
+    return date.year * 12 + (date.month - 1)
+
+def monthnum_to_date(mn):
+    year = int(mn) / 12
+    month = (mn - year * 12) + 1
+    return datetime.date(year,month,1)

904a9b1e1f23ec864e0eb5e2153b54637083e0c3
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-09-09 13:54:53 -0400

    1. Fixed bug re-setting up with template rundecks.  2. Put removal of modele-control files in a finally block to help get them deleted.  In tests, this does not trigger rebuilds.

--- lib/ectl/srcdir.py
+++ lib/ectl/srcdir.py
@@ -5,7 +5,13 @@ from ectl import xhash
 badRE = re.compile(r"\.git|\..*|doc|aux|init_cond|tests|pyext|pylib|decks|\.DS_Store|\.#.*|a\.out|.*\.o|spconfig\.py")
 
 def list_src_files(src_dir):
-    yield os.path.join(src_dir, 'CMakeLists.txt')
+    # Add build files in top-level ModelE directory (above model/)
+    modele_control = os.path.join(src_dir, 'modele-control.pyar')
+    if os.path.exists(modele_control):
+        yield modele_control
+    else:
+        yield os.path.join(src_dir, 'CMakeLists.txt')
+
     for top_dir in ('model', 'cmake'):
         for root, dirs, files in os.walk(os.path.join(src_dir, top_dir)):
             for file in files:

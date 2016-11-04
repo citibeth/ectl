citibeth/ectl

commit b98ebe2f61635a663e1af1d55f3d1c6a0e3d18a8

Bug fixes found running on discover.

lib/ectl/setup.py
@@ -231,7 +231,7 @@ def setup(run, rundeck=None, src=None, pkgbuild=False, rebuild=False, jobs=None)
             # If any of these files needs to change, we expect the user to
             # edit the pyar file.
             with ectl.util.working_dir(src):
-                if os.path.exists(MODELE_CONTROL_PYAR):
+                if os.path.exists(MODELE_CONTROL_PYAR) and not os.path.exists('CMakeLists.txt'):
                     print('Adding files from modele-control.pyar')
                     with open(MODELE_CONTROL_PYAR) as fin:
                         pyar.unpack_archive(fin, '.')
@@ -265,7 +265,9 @@ def setup(run, rundeck=None, src=None, pkgbuild=False, rebuild=False, jobs=None)
                         '-DCMAKE_INSTALL_PREFIX=%s' % pkg,
                         src]
 
-                    subprocess.check_call(cmd)
+                    env = dict(os.environ)
+                    del env['PYTHONPATH']    # Python2 in CMake build does its own thing
+                    subprocess.check_call(cmd, env=env)
                 except OSError as err:
                     sys.stderr.write(' '.join(cmd) + '\n')
                     sys.stderr.write('%s\n' % err)

lib/ectl/launch.py
@@ -22,7 +22,7 @@ def setup_parser(subparser):
         default=False,
         help='Overwrite run without asking (on start)')
     subparser.add_argument('-l', '--launcher', action='store', dest='launcher',
-        help='How to run the program')
+        help='How to run the program: mpi, slurm, slurm-debug')
 
     # -------- Arguments for SOME launchers
     subparser.add_argument('-np', '--ntasks', '-n', action='store', dest='np',

lib/ectl/launchers.py
@@ -143,12 +143,13 @@ class SlurmLauncher(object):
 
             proc = subprocess.Popen(sbatch_cmd,
                 stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-            (sout, serr) = proc.communicate('\n'.join([
+            batch_script = '\n'.join([
                 '#!/bin/sh',
                 '#',
                 '',
-                cmd_str]))
-            match = submittedRE.match(sout)
+                cmd_str])
+            (sout, serr) = proc.communicate(batch_script.encode())
+            match = submittedRE.match(sout.decode())
             sjobid = match.group(1)
 
             # Write the launch file
@@ -167,9 +168,9 @@ class SlurmLauncher(object):
         cmd = ['scontrol', 'show', 'jobid', '-dd', launch_txt['jobid']]
         (scontrol_txt, scontrol_err) = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         # Invalid Job ID: This job is long gone...
-        if invalidJobRE.match(scontrol_err) is not None:
+        if invalidJobRE.match(scontrol_err.decode()) is not None:
             return None
-        scontrol_dict = parse_scontrol(scontrol_txt)
+        scontrol_dict = parse_scontrol(scontrol_txt.decode())
 
         JobState = scontrol_dict['JobState']    
         return slurm_state_translation.get(JobState, None)

lib/ectl/rundeck/__init__.py
@@ -190,8 +190,8 @@ class Params(dict):
         if line is not None:
             line.param = param
 
-        # Full pathnames are already resolved...
-        if (type == FILE) and (len(os.path.split(value)[0]) > 0):
+        # Absolute pathnames are already resolved...
+        if (type == FILE) and (value[0] == '/'):
             param.rval = param.value
 
         return param

commit 56b0bbd36f9ea034037d0ccb0fb60c3b6831196d

Added back bugfix from  b98ebe2f accidentally removed in 9e23e8b9

lib/ectl/setup.py
@@ -265,7 +265,9 @@ def setup(run, rundeck=None, src=None, pkgbuild=False, rebuild=False, jobs=None,
                         '-DCMAKE_INSTALL_PREFIX=%s' % pkg,
                         src]
 
-                    subprocess.check_call(cmd)
+                    env = dict(os.environ)
+                    del env['PYTHONPATH']    # Python2 in CMake build does its own thing
+                    subprocess.check_call(cmd, env=env)
                 except OSError as err:
                     sys.stderr.write(' '.join(cmd) + '\n')
                     sys.stderr.write('%s\n' % err)
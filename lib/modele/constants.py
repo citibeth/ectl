fc866e9bd6ffc80a3462392b9b64b11fe02487fd
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2017-03-29 18:44:05 -0400

    Added more ModelE constants

--- lib/modele/constants.py
+++ lib/modele/constants.py
@@ -1,4 +1,11 @@
 #@param shi heat capacity of pure ice (at 0 C) [J kg-1 K-1]
 SHI = 2060.
+#@param shw heat capacity of water (at 20 C) [J kg-1 K-1]
+SHW = 4185.
 #@param lhm   latent heat of melt at 0 C 334590 [J kg-1]
 LHM = 3.34e5
+
+# !@param rhoi density of pure ice [kg m-3]
+RHOI = 916.6
+# Density of ettled snow [kg m-3]
+RHOS = 300.
